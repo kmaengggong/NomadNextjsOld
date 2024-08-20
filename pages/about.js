2539c4e449a30294ca5f36e7e02dea3bf2e53769
kmaengggong <kmaengggong@gmail.com>
2024-08-20 18:56:33 +0900

    GEO: 5.1 - 6.4

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+
+export default function About() {
+    return (
+        <div>
+            <NavBar />
+            <h1>about</h1>
+        </div>
+    );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+
+export default function Home() {
+    return (
+        <div>
+            <NavBar />
+            <h1>Hello</h1>
+        </div>
+    );
+}
